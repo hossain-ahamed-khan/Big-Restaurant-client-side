4f04df589dd568cc28d452b3fc890fe1bdffb8f5
hossain <hossainahamedk@gmail.com>
2024-06-02 16:45:11 +0600

    completed project and deploy on firebase

--- src/Pages/Home/Testimonials/Testimonials.jsx
+++ src/Pages/Home/Testimonials/Testimonials.jsx
@@ -19,7 +19,7 @@ const Testimonials = () => {
     const [reviews, setReviews] = useState([]);
 
     useEffect(() => {
-        fetch('http://localhost:5000/reviews')
+        fetch('https://bistro-boss-server-umber-two.vercel.app/reviews')
             .then(res => res.json())
             .then(data => setReviews(data))
     })
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -101,7 +101,7 @@ export const router = createBrowserRouter([
             {
                 path: "updateItem/:id",
                 element: <AdminRoute><UpdateItem></UpdateItem></AdminRoute>,
-                loader: ({ params }) => fetch(`http://localhost:5000/menu/${params.id}`)
+                loader: ({ params }) => fetch(`https://bistro-boss-server-umber-two.vercel.app/menu/${params.id}`)
             },
         ]
     },
